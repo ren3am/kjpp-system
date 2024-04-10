6bc4fd30ca4258dab176468e5cf29c45ad438a0b
ren3am <renat3am@gmail.com>
2024-04-10 22:07:41 +0700

    added base sidebar

--- projectkjpp/mysite/kjpp/urls.py
+++ projectkjpp/mysite/kjpp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('kertaskerja', views.kertaskerja, name='kertaskerja')
 ]
--- projectkjpp/mysite/kjpp/views.py
+++ projectkjpp/mysite/kjpp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login.html', {})
+
+def kertaskerja(request):
+    return render(request, 'kertaskerja.html', {})
