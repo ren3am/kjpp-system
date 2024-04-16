f4ea9f3a98f4ffab50e8c0c029b7776687a76a00
ren3am <renat3am@gmail.com>
2024-04-16 14:31:29 +0700

    added kertas kerta form

--- projectkjpp/mysite/kjpp/admin.py
+++ projectkjpp/mysite/kjpp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# import dari .models
+
+from .models import Pemberi_tugas
+
 # Register your models here.
+
+admin.site.register(Pemberi_tugas)
+
--- projectkjpp/mysite/kjpp/models.py
+++ projectkjpp/mysite/kjpp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# buat urusan database
+
+class Pemberi_tugas(models.Model):
+    alamat = models.CharField('Alamat Pemberi Tugas', max_length=300)
+    desa = models.CharField('Desa Pemberi Tugas', max_length=300)
+    kecamatan = models.CharField('Kecamatan Pemberi Tugas', max_length=300)
+    kabupaten = models.CharField('Kabupaten Pemberi Tugas', max_length=300)
+    propinsi = models.CharField('Propinsi Pemberi Tugas', max_length=300)
+
+    def __str__(self):
+        return self.alamat
+    
+    #buat munculin list alamat di admin area
